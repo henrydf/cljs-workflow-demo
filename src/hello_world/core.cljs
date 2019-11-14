da11751111ab174a5d8164ede22a76155090b931
Henry <henry.ding@3vyd.com>
2019-11-14 11:01:36 +0800

    first demo

--- src/hello_world/core.cljs
+++ src/hello_world/core.cljs
@@ -7,17 +7,48 @@
 
 (println "Hello world!")
 
-#_(defn use-3d-engine []
-  (a/go))
+(defonce service-url "http://39.98.137.244:7777/get-sdk-url/sdk.js")
+(defonce scene-url "http://39.98.137.244:7777/demo/scene/scene.json")
+(defonce dynamic-material-url "http://39.98.137.244:7777/demo/replace.json")
+(defonce demo-texture-url "http://39.98.137.244:7777/demo/fabric/JNS496187.jpg")
+(defonce model-urls #js ["http://39.98.137.244:7777/demo/chenyi.json"
+                         "http://39.98.137.244:7777/demo/dp.json"
+                         "http://39.98.137.244:7777/demo/dp_lz.json"
+                         "http://39.98.137.244:7777/demo/dp_2k.json"
+                         "http://39.98.137.244:7777/demo/dp_2k_ky.json"
+                         "http://39.98.137.244:7777/demo/dp_pbt_7.5.json"
+                         "http://39.98.137.244:7777/demo/dp_pbt_7.5_ky.json"
+                         "http://39.98.137.244:7777/demo/dp_1k.json"
+                         "http://39.98.137.244:7777/demo/dp_1k_ky.json"])
 
-(defn aaa []
-  (js/Promise. #(%1 "resolved js promise")))
+(defn js-fetch [url]
+  (-> url
+      js/fetch
+      (.then #(.json %))))
+
+(defn import-script [src]
+  (js/Promise.
+   (fn [resolve reject]
+     (let [script (.createElement js/document "script")]
+       (goog.object/set script "onload" #(resolve js/__3d_sdk__))
+       (goog.object/set script "onerror" reject)
+       (goog.object/set script "src" src)
+       (.appendChild js/document.body script)))))
+
 
 (defn init-task [state]
   (a/go
-    #_(->> (wf/take! :sth-in-futher) a/<! (swap! state assoc :1step))
-    (->> (wf/call! aaa) a/<! (swap! state assoc :2step))
-    (prn state)))
+    (->> (wf/call! js-fetch service-url) a/<! (swap! state assoc :sdk-url))
+    (->> (wf/call! import-script (-> @state :sdk-url .-url)) a/<! (swap! state assoc :sdk))
+    (->> (wf/call! (-> (:sdk @state) (.init "3d-container" scene-url)) a/<!))
+    (->> (wf/call! (.loadDynamicMaterial (:sdk @state) dynamic-material-url)) a/<!)
+    (->> (wf/call! (.updateBaseTexture (:sdk @state) demo-texture-url 4 1)) a/<!)
+    (->> (wf/call! (.loadModels (:sdk @state) model-urls)) a/<! (swap! state assoc :models))
+    (.forEach (:models @state)
+              (fn [m]
+                (.showObj (:sdk @state) (.-id m) (.-key m))
+                (.freshFabric (:sdk @state) (.-id m) (.-key m))))
+    (prn @state)))
 (wf/fork! init-task)
 
 ; (a/go
